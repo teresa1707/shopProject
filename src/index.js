6ffc33f70d86909fc75f9427f4d557a48dc76343
Teresa <teriv1707@gmail.com>
2022-05-26 17:34:17 +0200

    add

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const h1 = <h1>Hello world</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(h1);
