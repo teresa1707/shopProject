3cacc04a03c92941349d01a167dea832d8de8e74
Teresa <teriv1707@gmail.com>
2022-05-26 18:07:19 +0200

    added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,72 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const h1 = <h1>Hello world</h1>
+// // const h1 = <h1>Hello world</h1>
+// // const el = (<div>
+// //   <div>
+// //     <ul>
+// //       <li>list1
+// //       </li>
+// //       <li>list2
+// //       </li>
+// //       <li>list3
+// //       </li>
+// //     </ul>
+// //   </div>
+// //   <p>lorem
+// //   </p>
+// // </div>)
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(h1);
+// // const root = ReactDOM.createRoot(document.getElementById("root"));
+// // root.render(h1);
+
+// function App() {
+// return (<div>
+//   <h1>Hello App.js</h1>;
+//   <ul>
+//     <li>list1
+//       </li>
+//        <li>list2
+//        </li>
+//        <li>list3
+//        </li>
+//      </ul>
+//   </div>)
+// }
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(<App />);
+
+//  function App() {
+//   return (<React.Fragment>
+//     <h1>Hello App.js</h1>
+//     <ul>
+//       <li>list1
+//         </li>
+//          <li>list2
+//          </li>
+//          <li>list3
+//          </li>
+//        </ul>
+//        </React.Fragment>) 
+//        ;
+//   }
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+//    root.render(<App />);
+
+   
+ function App() {
+  return (<>
+    <h1>Hello App.js</h1>
+    <ul>
+      <li>list1
+        </li>
+         <li>list2
+         </li>
+         <li>list3
+         </li>
+       </ul>
+       </>) 
+       ;
+  }
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+   root.render(<App />);
