04a9a46360a0fdbfb9860e0cd48e20655317d3b7
Teresa <teriv1707@gmail.com>
2022-05-31 17:34:18 +0200

    added material

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { Header } from 'container/Header/Header'
 import { Main } from 'container/Main/Main'
 import { Footer } from 'container/Footer/Footer'
+import { CssBaseline } from '@mui/material'
 
 export const App = () => {
     return (
         <>
+            {' '}
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
