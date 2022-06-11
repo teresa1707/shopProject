da6cde2cb40fda55ad4a3b3bbb93441077e6e173
Teresa <teriv1707@gmail.com>
2022-06-11 19:46:59 +0200

    add

--- src/container/App/App.js
+++ src/container/App/App.js
@@ -3,7 +3,6 @@ import { Header } from 'container/Header/Header'
 import { Main } from 'container/Main/Main'
 import { Footer } from 'container/Footer/Footer'
 import { CssBaseline } from '@mui/material'
-import productsArray from 'utils/productsArray'
 
 export const App = () => {
     const [cartData, setCartData] = useState({ count: 10, price: 0 })
